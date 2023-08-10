c7e572f3df37266fa307cde9876d126ce85d6f07
mejdikazdarlli <89795915+mejdikazdarlli@users.noreply.github.com>
2023-08-10 01:44:19 +0100

    update all video for mobile and desktop and other css update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,72 @@
+let isMobile = false;
+if (
+  /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(
+    navigator.userAgent,
+  )
+  || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+    navigator.userAgent.substring(0, 4),
+  )
+) {
+  isMobile = true;
+}
+var currentURL = window.location.href;
+var pathname = new URL(currentURL).pathname;
+var htmlPageName = pathname.substring(pathname.lastIndexOf("/") + 1).split(".")[0];
+
+switch (htmlPageName) {
+  case "industries":
+    let industriesVideo = document.querySelector(".industriesVideo")
+    let PivotPainter_vid = document.querySelector(".PivotPainterSRC")
+    let ArtGallery_vid = document.querySelector(".ArtGallerySRC")
+    let TLI_vid = document.querySelector(".TLISRC")
+    let Fashion_vid = document.querySelector(".FashionSRC")
+    if(isMobile)
+    {
+      PivotPainter_vid.src = "images/Pivot-Painter_mob.webm"
+      document.getElementById("PivotPainter_poster").poster = "images/PivotPainter_mob.webp"
+
+      ArtGallery_vid.src = "images/ArtGallery_mob.webm"
+      document.getElementById("ArtGallery_poster").poster = "images/ArtGallery_mob.webp"
+
+      TLI_vid.src = "images/TLI-3D_mob.webm"
+      document.getElementById("TLI_poster").poster = "images/TLI-3d_mob.webp"
+
+      Fashion_vid.src = "images/Fashion_CLIP_mob.webm"
+      document.getElementById("Fashion_poster").poster = "images/Fashion_CLIP_mob.webp"
+
+      industriesVideo.load();
+    }
+    else
+    {
+      PivotPainter_vid.src = "images/Pivot-Painter.webm"
+      document.getElementById("PivotPainter_poster").poster = "images/PivotPainter.webp"
+
+      ArtGallery_vid.src = "images/ArtGallery.webm"
+      document.getElementById("ArtGallery_poster").poster = "images/ArtGallery.webp"
+
+      TLI_vid.src = "images/TLI-3D.webm"
+      document.getElementById("TLI_poster").poster = "images/TLI-3d.webp"
+
+      Fashion_vid.src = "images/Fashion_CLIP.webm"
+      document.getElementById("Fashion_poster").poster = "images/Fashion_CLIP.webp"
+    }
+    
+    break;
+    case "our_platform":
+      let ourplatformvideo = document.querySelector(".ourplatformvideo")
+      let Platform_vid = document.querySelector(".Platform_vidSRC")
+      if(isMobile)
+      {
+        Platform_vid.src ="images/built-in-asset-library_Trim_mob.webm"
+        ourplatformvideo.load();
+      }
+      else
+      {
+        Platform_vid.src ="images/built-in-asset-library_Trim.webm"
+      }
+      
+      break;
+}
 
 
 window.addEventListener("scroll", function () {
@@ -225,7 +294,7 @@ function togglenav() {
   nav.classList.add("sticky");
   if (x.style.display == "none") {
     x.style.display = "flex";
-    myimg.src = "./images/mocartlogoblack.png";
+    //myimg.src = "./images/mocartlogoblack.png";
     h2.style.display = "none"
     h1.style.transform = "rotate(45deg)"
     h3.style.transform = "rotate(-45deg)"
@@ -234,7 +303,7 @@ function togglenav() {
   } else if (x.style.display == "flex") {
     x.style.display = "none";
     nav.classList.remove("sticky");
-    myimg.src = "./images/mocartlogo.png";
+    //myimg.src = "./images/mocartlogo.png";
     h1.style.transform = "rotate(0deg)"
     h3.style.transform = "rotate(0deg)"
     h2.style.display = "block"
