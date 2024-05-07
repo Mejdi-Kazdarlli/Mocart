09643db7dcd5f2f720fe08b5c8964edb61d39692
mejdikazdarlli <89795915+mejdikazdarlli@users.noreply.github.com>
2024-05-07 21:56:45 +0100

    remove all files

--- js/ViewerPlatform.js
+++
@@ -1,8 +0,0 @@
-import { product3d} from './FrontScene.js';
-function _(elm){return document.getElementById(elm)}
-const productScene = new product3d(_("viewer"));
-productScene.initScene("backpack",'brown_photostudio.hdr');
-productScene.animate();
-_("viewer").addEventListener( 'pointerdown', function () {
-    _("SVG360").style.display = "none"
-} );
--- js/lib.js
+++
@@ -1,5 +0,0 @@
-export  function getelmentFromRoot(a,b) {
-    return {
-        Elm: document.querySelector(a).shadowRoot.querySelector(b)
-      };
-  }
